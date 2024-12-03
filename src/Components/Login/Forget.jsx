c8ba0cd6218de37d656daa84edc5b3f47bd0b697
Vinzecta <vinh.trantrung@hcmut.edu.vn>
2024-12-03 09:59:19 +0700

    Add files via upload

--- src/Components/Login/Forget.jsx
+++ src/Components/Login/Forget.jsx
@@ -3,15 +3,11 @@ import React, { useState } from "react";
 import LogHeader from "./LoginHeader";
 import LFooter from "./LFooter";
 import { Link } from "react-router-dom";
-import { useNavigate, useLocation } from "react-router-dom";
 function Forget(){
   const [email, setEmail] = useState("");
   const [oldPassword, setPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [verifyPassword, setVerifyPassword] = useState("");
-
-    const location = useLocation();
-  const { userType } = location.state || {}; 
     return(
       <>
         <LogHeader />
@@ -65,15 +61,13 @@ function Forget(){
                  type="submit"
                  className="w-full bg-[#7077A1] text-white py-2 px-4 rounded hover:bg-[#F6B17A] mt-4"
                >
-                 <Link to = "/select">
                  Reset Password
-                 </Link>
                </button>
              <div className="flex justify-between mt-4 mb-2">
                <button
                  className="text-[#7077A1] hover:text-[#2D3250] text-sm"
                >
-                 <Link to ="/loginform" state={{ userType }}>
+                 <Link to ="/select">
                  Back to Login
                  </Link> 
                </button>
